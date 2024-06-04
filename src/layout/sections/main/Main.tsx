b8f89e778bcfe8fdd63359a8d8dd5380fadf1e62
Maryie <mashenka.zinkevich0245@gmail.com>
2024-06-04 13:05:22 +0300

    Code was updated

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Main = () => {
+    return (
+        <div>
+            <div>
+                <span>Hi There</span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
+
+// h1 is very necessary for promotion. ceo ai scrolling trough h1 tag and searching for separate words 
+//that are usefull, that are known, that user wants exactly this to findAllByAltText.
