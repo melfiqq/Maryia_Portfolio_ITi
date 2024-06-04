23097616f7d3ffdf4463af437e9cd920130b8c8b
Maryie <mashenka.zinkevich0245@gmail.com>
2024-06-04 14:55:59 +0300

    Code was updated

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,15 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import photo from "../../../assets/images/me-photo.jpg"
+import { FlexWrapper } from "../../../components/FlexWrapper";
 
 export const Main = () => {
     return (
-        <div>
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"}>
             <div>
                 <span>Hi There</span>
-                <span></span>
+                <h2>I am Maryia Zinkevich.</h2>
+                <h1>A Web Developer.</h1>
             </div>
-        </div>
+            <Photo  src={photo} alt=""/>
+        
+            </FlexWrapper>
+        </StyledMain>
     )
 }
+
+const StyledMain = styled.div`
+min-height: 100vh;
+background-color: #fff5e7; 
+`
+
+const Photo = styled.img`
+width: 390px;
+height: 388px;
+object-fit: cover;
+`
 
 // h1 is very necessary for promotion. ceo ai scrolling trough h1 tag and searching for separate words 
 //that are usefull, that are known, that user wants exactly this to findAllByAltText.
