17b03fc1a9095a57a5ce99ef45fda4a5e5d0a151
Maryie <mashenka.zinkevich0245@gmail.com>
2024-06-10 17:55:49 +0300

    Code were updated

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -3,6 +3,10 @@ import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Menu } from "../../../components/menu/Menu";
 import { FlexWrapper } from "../../../components/FlexWrapper";
+import { Work } from "./work/Work";
+import socialImg from './../../../assets/images/proj-1-.jpg'
+import timerlImg from './../../../assets/images/proj-2-.jpg'
+
 
 const worksItems = ["ALL", "LANDING PAGE", "REACT", "SPA"]
 
@@ -13,7 +17,13 @@ export const Works = () => {
             <SectionTitle>My Work</SectionTitle>
             <Menu menuItems={worksItems}/>
             <FlexWrapper>
-                
+                <Work title={"Social Network"} 
+                text={"OOOOOOOOOOOOOOOOOOOOO"} 
+                src={socialImg}/>
+                <Work title={"Timer"} 
+                text={"AAAAAAAAAAAAAAAAAAAAAAA"} 
+                src={timerlImg}/>
+
             </FlexWrapper>
         </StyledWorks>
     )
