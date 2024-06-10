de8f00d94567dce260addc3d83e23cfb0f6f351f
Maryie <mashenka.zinkevich0245@gmail.com>
2024-06-10 19:13:26 +0300

    Code were updated

---
+++ src/layout/sections/slogan/Slogan.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import { SectionTitle } from '../../../components/SectionTitle';
+import { Button } from '../../../components/Button';
+
+export const Slogan = () => {
+    return (
+        <StyledSlogan>
+            <SectionTitle>I Am Available For Freelance.</SectionTitle>
+            <Button>Hire Me.</Button>
+        </StyledSlogan>
+    );
+};
+
+
+const StyledSlogan = styled.section`
+min-height: 30vh;
+background-color: #ffe1f5;
+
+`
